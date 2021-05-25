3e1335166d84c9d6693657b40cc037e8f9ce16cd
The Dang <thedangthanhh@gmail.com>
2021-05-26 00:37:47 +0700

    Login

---
+++ Android/Project/app/src/main/java/com/example/project/network/LoginResponse.java
@@ -0,0 +1,30 @@
+package com.example.project.network;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class LoginResponse {
+    @SerializedName("token")
+    @Expose
+    private String token;
+
+    @SerializedName("username")
+    @Expose
+    private String username;
+
+    @SerializedName("role")
+    @Expose
+    private String role;
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
