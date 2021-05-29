ea920ba92671e34c6a07f6c97aa25f815344ddfd
NgoanHuynh <58302202+NgoanHuynh@users.noreply.github.com>
2021-05-29 23:52:26 +0700

    update uncomplete assignment

---
+++ Android/Project/app/src/main/java/com/example/project/models/Assignment.java
@@ -0,0 +1,62 @@
+package com.example.project.models;
+
+public class Assignment {
+    private int classID, moduleID;
+    private String trainerID, moduleName, className, trainerName, registrationCode;
+
+    public int getClassID() {
+        return classID;
+    }
+
+    public void setClassID(int classID) {
+        this.classID = classID;
+    }
+
+    public int getModuleID() {
+        return moduleID;
+    }
+
+    public void setModuleID(int moduleID) {
+        this.moduleID = moduleID;
+    }
+
+    public String getTrainerID() {
+        return trainerID;
+    }
+
+    public void setTrainerID(String trainerID) {
+        this.trainerID = trainerID;
+    }
+
+    public String getModuleName() {
+        return moduleName;
+    }
+
+    public void setModuleName(String moduleName) {
+        this.moduleName = moduleName;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getTrainerName() {
+        return trainerName;
+    }
+
+    public void setTrainerName(String trainerName) {
+        this.trainerName = trainerName;
+    }
+
+    public String getRegistrationCode() {
+        return registrationCode;
+    }
+
+    public void setRegistrationCode(String registrationCode) {
+        this.registrationCode = registrationCode;
+    }
+}
